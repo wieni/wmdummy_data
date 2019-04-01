8dc195492c5670378f3abf25163e6ee2ba0f7452
Pham Van den Eynden <pham_see@hotmail.com>
2019-04-01 11:22:44 +0200

    added more to dummydatabase

--- src/DummyDataBase.php
+++ src/DummyDataBase.php
@@ -3,9 +3,26 @@
 namespace Drupal\wmdummy_data;
 
 use Drupal\Component\Plugin\PluginBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\wmdummy_data\Annotation\DummyData;
+use Faker\Generator as Faker;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-abstract class DummyDataBase extends PluginBase implements DummyDataInterface
+abstract class DummyDataBase extends PluginBase implements DummyDataInterface, ContainerFactoryPluginInterface
 {
+    /** @var Faker */
+    protected $faker;
+
+    public function __construct(
+        array $configuration,
+        string $pluginId,
+        $pluginDefinition,
+        Faker $faker
+    ) {
+        parent::__construct($configuration, $pluginId, $pluginDefinition);
+        $this->faker = $faker;
+    }
+
     abstract public function generate(): array;
 
     public function getKey(): string
@@ -16,5 +33,19 @@ abstract class DummyDataBase extends PluginBase implements DummyDataInterface
     public function getLangcode(): string
     {
         return $this->pluginDefinition['langcode'];
+    }
+
+    public static function create(
+        ContainerInterface $container,
+        array $configuration,
+        $pluginId,
+        $pluginDefinition
+    ) {
+        return new static(
+            $configuration,
+            $pluginId,
+            $pluginDefinition,
+            $container->get('wmdummy_data.faker')
+        );
     }
 }
